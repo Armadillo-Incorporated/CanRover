e8250fb282e287b293e44e7c872e72787312faf1
screamprobation <knight.costa47@gmail.com>
2016-01-15 17:20:19 +0000

    Change logo, add text to sections, change fonts
    Sections changed: About, Team, Missions and Contact Us

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,19 +17,19 @@
 
     <div class="row" id="about" data-magellan-target="about">
     <div class="small-9 small-centered columns">
-      <section>
-          <p>
-              <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat aperiam rerum officiis velit, inventore perferendis dolores odit! Sapiente, consequatur laudantium labore rem consequuntur voluptatibus, distinctio quasi impedit, natus nesciunt nisi?</span>
-              <span>Adipisci, sint? Eos quia perferendis quam, sequi velit fugit excepturi similique, accusamus pariatur odio ab quibusdam quisquam. Quasi, beatae, incidunt. Necessitatibus commodi fugiat provident unde facilis dolor voluptates ipsam ex?</span>
-              <span>Sit delectus tempore, unde reiciendis placeat? Voluptate vero id voluptatibus maiores nulla earum dignissimos consectetur, laboriosam beatae labore corporis vel aspernatur magnam placeat, modi quo voluptates aut at, recusandae ut?</span>
-              <span>Nostrum ea minus nam sunt nihil. Fugiat possimus amet quis ipsam dicta nisi quasi, adipisci nulla quia, minus asperiores, dolor repudiandae magnam obcaecati alias expedita, aspernatur voluptates consectetur sit nihil!</span>
-              <span>Id incidunt, delectus eveniet facere, laudantium at ipsam vel ipsum veniam sapiente, quaerat nihil molestias iusto accusamus hic! Consectetur aspernatur exercitationem inventore ipsum beatae quos porro, dolor incidunt, doloribus voluptatem.</span>
-              <span>Ratione autem eius ipsam veniam aliquid vel dolores tempore facere possimus. Culpa corporis, qui sed accusantium hic animi est optio reprehenderit soluta similique ad incidunt quibusdam repellat aut nihil voluptatibus.</span>
-              <span>Deserunt suscipit adipisci vero, minima qui ipsam sequi dolorem magnam! Nostrum natus, nisi voluptatem necessitatibus tenetur, ducimus. Quasi animi, obcaecati suscipit nam ea id! Quod amet tempora cumque accusantium, saepe.</span>
-              <span>Necessitatibus id similique assumenda sapiente repellat fugiat, atque velit quidem minima architecto labore natus, placeat nostrum, ipsam ratione maiores ab dolorum tenetur quibusdam. Laudantium, iste, natus asperiores culpa cum magni!</span>
-              <span>Aliquid, dolorum, quod. Ex ea rem modi enim atque, veniam reprehenderit id libero, dolores culpa at aperiam, et quibusdam iusto cumque nobis velit sit mollitia aut sapiente quos delectus consectetur.</span>
-              <span>Nobis quod at facere maiores aperiam totam dicta, saepe ipsum provident, accusamus perferendis possimus voluptas cupiditate voluptates nostrum necessitatibus veniam iure! Numquam earum at natus officia voluptatum quam vitae ad?</span>
-          </p>
+        <section>
+            <p>
+              CanSats in Europe is an initiative of the European Space Agency (ESA), which also supports national CanSat events with the help of local organisations. These organisations are collaborating to increase the support for CanSat activities in Europe.
+            </p>
+            <p>
+                A CanSat is a simulation of a real satellite, integrated within the volume and shape of a soft drink can. The challenge for the students is to fit all the major subsystems found in a satellite, such as power, sensors and a communication system, into this minimal volume. The CanSat is then launched to an altitude of a few hundred metres by a rocket or dropped from a platform or captive balloon and its mission begins: to carry out a scientific experiment and achieve a safe landing.
+              </p>
+            <p>
+                CanSats offer a unique opportunity for students to have a first practical experience of a real space project. They are responsible for all aspects: designing the CanSat, selecting its mission, integrating the components, testing, preparing for launch and then analysing the data.
+            </p>
+            <p>
+                Teams must be able to design and build a space-type system, following the approved competition guide, and then compete against each at the end of two semesters to determine the winners. Rockets will be provided but teams are responsible for funding the construction of their CanSat and all travel/lodging expenses.
+            </p>
       </section>
       </div>
   </div>
@@ -43,17 +43,17 @@
       <div class="row" id="team" data-magellan-target="team">
       <div class="small-9 small-centered columns">
       <section>
-          <p>
-              <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed assumenda obcaecati velit magnam quibusdam veritatis suscipit molestiae optio, amet corporis, provident et. Omnis, amet, ad. Vitae vel qui a, neque.</span>
-              <span>Consequuntur ipsa cupiditate repellat corporis soluta doloremque amet, quas quaerat aperiam explicabo inventore facere ipsum atque optio incidunt. Quibusdam quam odio nisi voluptatem fugit non ipsum tenetur corporis blanditiis iure.</span>
-              <span>Ipsam ipsa quas vitae in aliquid quis numquam atque ipsum delectus deleniti et, officia praesentium nihil recusandae eveniet sunt rerum ratione molestiae doloribus quaerat voluptatem optio molestias quam, reprehenderit! Facere!</span>
-              <span>Optio, iure aut magnam, voluptatum, iusto reprehenderit blanditiis sequi consectetur eaque impedit unde quo? Atque impedit optio sunt maiores omnis sint beatae inventore, sequi perspiciatis, magnam in hic tenetur harum.</span>
-              <span>Iusto accusantium adipisci odit earum nemo excepturi cumque blanditiis impedit tempore, laboriosam inventore, sapiente veritatis quas quidem rem provident eius sit? Hic non tempora veritatis quidem aut, eligendi, placeat neque?</span>
-              <span>Sed illo voluptate, quo totam adipisci suscipit ipsam placeat sequi. Omnis officia repellat illo vitae nulla, praesentium atque impedit odit nisi quibusdam iusto accusantium. Quas ab ducimus delectus, repellat sunt.</span>
-              <span>Numquam, officiis doloribus quisquam obcaecati amet aliquid, nulla sed quis soluta harum optio voluptate facere adipisci inventore pariatur, velit quos dicta debitis sit laudantium molestiae totam similique cupiditate. Voluptatum, excepturi.</span>
-              <span>Sunt, quis cumque temporibus sit atque tenetur, harum neque, aperiam quo mollitia numquam libero assumenda. Animi, corporis quod sit mollitia alias, iusto cupiditate delectus modi dicta quas similique quisquam deserunt!</span>
-              <span>Blanditiis numquam voluptatum quae, quas beatae, ullam possimus eveniet, iure, provident molestiae repudiandae. Dolor ut, quas repellat asperiores? Excepturi ab eum beatae, dignissimos nesciunt tenetur asperiores aspernatur, minus dicta necessitatibus?</span>
-              <span>Numquam placeat adipisci odio tenetur incidunt cumque quaerat nulla, excepturi voluptas saepe perferendis error quibusdam praesentium et, animi ad autem accusamus modi distinctio repudiandae officia veritatis in. Temporibus, ullam, ipsa.</span>
+            <p>
+                We are a team of six students, all from the same class, class 12ºE from Escola Secundaria João de Deus, Faro. Faro is the district capital of the Algarve region in the South of Portugal.
+            </p>
+            <p>
+                This is our first experience in building a satellite from scratch and it is also our first time dealing with this type of components and micro-components. Our main objective is to walk this road step by step until the end, the opportunity to visit ESA (European Space Agency) is our greatest motivation.
+            </p>
+            <p>
+                We all have a genuine interest in science, all of us want to follow careers in the field of space engineering, electrical engineering and architecture, therefore this is also a challenge to our abilities, during this adventure we will have a chance to test our management skills, our scientific knowledge, our team spirit and our will to take this project to the end.
+            </p>
+            <p>
+                The first objective is to win the National Final witch will take place on April 16th and 17th in Torres Vedras and qualify for the International Challenge.
           </p>
       </section>
   </div>
@@ -68,18 +68,25 @@
   <div class="row" id="missions" data-magellan-target="missions">
       <div class="small-9 small-centered columns">
         <section>
-          <p>
-              <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, autem expedita totam consequuntur harum explicabo magnam quam distinctio fuga, inventore voluptatem in corporis nesciunt tempora, quas est molestiae ut nam?</span>
-              <span>Commodi sed dolor porro quo tenetur esse tempore illum doloribus, totam eius quos, similique, nisi aut, deleniti consectetur accusamus. Ullam id voluptas impedit quae aut enim eius corporis, dolores eligendi.</span>
-              <span>Voluptatem possimus, id ipsam consequuntur voluptatum dolore iusto porro quasi accusamus doloremque sunt illum, fugiat eligendi magnam atque quia vel adipisci. Nulla quam fugiat nemo in doloremque quaerat fugit expedita.</span>
-              <span>Fuga repellendus amet necessitatibus totam laudantium, optio adipisci blanditiis ad explicabo dicta tempora facere eum placeat, nemo magnam. Repellendus quaerat, qui necessitatibus eos fugiat doloremque facere natus, est doloribus recusandae?</span>
-              <span>Expedita fugiat doloribus beatae, ratione tempore saepe perspiciatis, quibusdam repudiandae tempora culpa ullam nostrum alias error atque voluptatum, explicabo possimus. Enim illum voluptas voluptatem dignissimos expedita, iusto itaque nisi accusamus!</span>
-              <span>Nostrum distinctio veritatis laboriosam dicta, a nisi consectetur tempora blanditiis voluptatem rerum voluptatibus illum quod quae dolore nemo doloremque cumque, ducimus aut aliquid at obcaecati! Vitae delectus in commodi, rem.</span>
-              <span>Nam assumenda a, tenetur magnam ad dignissimos incidunt velit, fugiat atque? Sequi, culpa blanditiis eligendi, quos tempore nesciunt laboriosam odio, saepe minima cumque vitae repellendus nisi accusamus magnam explicabo expedita?</span>
-              <span>Accusamus asperiores suscipit aliquam expedita. Qui nihil, libero excepturi soluta ad non, sunt tenetur amet debitis rerum facere, blanditiis itaque fugit maxime aliquid cumque aut veniam illo laudantium? Velit, esse.</span>
-              <span>Optio nulla minus iure vitae beatae omnis sequi qui voluptas quae velit. Nam, itaque, praesentium! Impedit explicabo tenetur facilis, eum fugiat earum quae dolorem! Iure est sapiente perferendis beatae cumque.</span>
-              <span>Laborum corrupti dolore consectetur iure nulla ea rem veniam sunt reprehenderit natus tempore illo delectus minus dolorum, officiis! Commodi possimus vero mollitia animi deleniti et omnis quibusdam amet tempora. Ut?</span>
-          </p>
+            <p>
+              Each cansat Project must complete 2 separate missions: a primary mission and a secondary mission.
+            </p>
+            <p>
+                The team must build a CanSat and program it to accomplish the compulsory primary mission, as follows:
+            </p>
+            <p>
+                After release and during descent, the CanSat shall measure the following parameters and transmit the data as telemetry at least once every second to the ground station:
+            </p>
+            <ul>
+                <li>Air temperature</li>
+                <li>Air pressure</li>
+            </ul>
+            <p>
+                It must be possible for the team to analyse the data obtained (for example, make a calculation of altitude) and display it in graphs (for example, altitude vs. time and temperature vs. altitude).
+            </p>
+            <p>
+                The secondary mission for the CanSat must be selected by the team. It can be based on other satellite missions, a perceived need for scientific data for a specific project, a technology demonstration for a student-designed component, or any other mission that would fit the CanSat’s capabilities.
+            </p>
       </section>
   </div>
   </div>
@@ -169,16 +176,7 @@
       <div class="small-9 small-centered columns" id="contactus" data-magellan-target="contactus">
         <section>
           <p>
-              <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, autem expedita totam consequuntur harum explicabo magnam quam distinctio fuga, inventore voluptatem in corporis nesciunt tempora, quas est molestiae ut nam?</span>
-              <span>Commodi sed dolor porro quo tenetur esse tempore illum doloribus, totam eius quos, similique, nisi aut, deleniti consectetur accusamus. Ullam id voluptas impedit quae aut enim eius corporis, dolores eligendi.</span>
-              <span>Voluptatem possimus, id ipsam consequuntur voluptatum dolore iusto porro quasi accusamus doloremque sunt illum, fugiat eligendi magnam atque quia vel adipisci. Nulla quam fugiat nemo in doloremque quaerat fugit expedita.</span>
-              <span>Fuga repellendus amet necessitatibus totam laudantium, optio adipisci blanditiis ad explicabo dicta tempora facere eum placeat, nemo magnam. Repellendus quaerat, qui necessitatibus eos fugiat doloremque facere natus, est doloribus recusandae?</span>
-              <span>Expedita fugiat doloribus beatae, ratione tempore saepe perspiciatis, quibusdam repudiandae tempora culpa ullam nostrum alias error atque voluptatum, explicabo possimus. Enim illum voluptas voluptatem dignissimos expedita, iusto itaque nisi accusamus!</span>
-              <span>Nostrum distinctio veritatis laboriosam dicta, a nisi consectetur tempora blanditiis voluptatem rerum voluptatibus illum quod quae dolore nemo doloremque cumque, ducimus aut aliquid at obcaecati! Vitae delectus in commodi, rem.</span>
-              <span>Nam assumenda a, tenetur magnam ad dignissimos incidunt velit, fugiat atque? Sequi, culpa blanditiis eligendi, quos tempore nesciunt laboriosam odio, saepe minima cumque vitae repellendus nisi accusamus magnam explicabo expedita?</span>
-              <span>Accusamus asperiores suscipit aliquam expedita. Qui nihil, libero excepturi soluta ad non, sunt tenetur amet debitis rerum facere, blanditiis itaque fugit maxime aliquid cumque aut veniam illo laudantium? Velit, esse.</span>
-              <span>Optio nulla minus iure vitae beatae omnis sequi qui voluptas quae velit. Nam, itaque, praesentium! Impedit explicabo tenetur facilis, eum fugiat earum quae dolorem! Iure est sapiente perferendis beatae cumque.</span>
-              <span>Laborum corrupti dolore consectetur iure nulla ea rem veniam sunt reprehenderit natus tempore illo delectus minus dolorum, officiis! Commodi possimus vero mollitia animi deleniti et omnis quibusdam amet tempora. Ut?</span>
+            For any additional explanation, clarification, feedback or any other type of contact please send us an email to <a href="mailto:canrover2016@gmail.com">canrover2016@gmail.com</a>
           </p>
       </section>
   </div>
