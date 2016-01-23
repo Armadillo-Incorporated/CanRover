db2d326b9cd38e2186cf522215fa1e176c57e36f
screamprobation <knight.costa47@gmail.com>
2016-01-23 00:16:06 +0000

    Remove unnecessary h1 tag

--- resources/views/pages/report.blade.php
+++ resources/views/pages/report.blade.php
@@ -3,14 +3,13 @@
 @section('content')
     <div class="title">
       <div class="small-12 medium-10 small-centered medium-centered columns">
-          <h1 class="text-center">Full Report</h1>
+          <h1 class="text-center">Full Report Document</h1>
       </div>
     </div>
 
     <div class="row align-middle">
         <div class="small-12 medium-10 small-centered medium-centered columns">
             <section>
-                <h1>Full Report Document</h1>
                 <div class="button primary"><a href="/Full_Report.pdf" style="color: white;">Download PDF</a></div>
             </section>
         </div>
